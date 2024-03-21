c56b42948387744aa99386225fec4c8dc801dbf0
mehebul02 <mehebul2123@gmail.com>
2024-03-21 13:13:10 +0600

    go back button add

--- src/components/Header/users/user/User.jsx
+++ src/components/Header/users/user/User.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const User =({user})=>{
     const {id,name,email,phone} = user
@@ -7,12 +7,16 @@ const User =({user})=>{
         padding:'10px',
         borderRadius:'10px'
     }
+    const navigate = useNavigate()
+    const handleuserDetails=()=>{
+        navigate(`/user/${id}`)
+    }
     return(
         <div style={userStyle} className="space-y-6 shadow-md">
             <h1 className="text-2xl">{name}</h1>
             <p>Email:{email}</p>
             <p>Phone:{phone}</p>
-           <Link to={`/user/${id}`}> <button className="btn bg-cyan-700 text-white font-bold hover:bg-amber-600">User Details</button></Link>
+            <button onClick={handleuserDetails} className="btn bg-cyan-700 text-white font-bold hover:bg-amber-600 mt-2">User Details</button>
 
         </div>
     )
--- src/components/Header/users/user/userDetails/UserDetails.jsx
+++ src/components/Header/users/user/userDetails/UserDetails.jsx
@@ -1,12 +1,30 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const UserDetails =()=>{
 const user = useLoaderData()
-const {name,email,phone} =user
+const {id,name,email,phone,website} =user
+const userStyle={
+    border:'2px solid red',
+    padding:'10px',
+    borderRadius:'10px',
+    width:'200px',
+    textAlign:'center'
+}
+const navigate =useNavigate()
+const handleGoBack =()=>{
+    navigate(-1)
+}
     return(
         
-        <div>
-            <h1>{name}</h1>
+        <div className="">
+           <div className="text-center border-2 border-red-500 max-w-2xl mx-auto mt-10">
+            <h1>Post Id:{id}</h1>
+           <h1 className="text-2xl">Name:{name}</h1>
+           <p>Email:{email}</p>
+           <p>Phone:{phone}</p>
+           <p>Website:{website}</p>
+           <button onClick={handleGoBack} className="btn bg-cyan-600 text-xl text-white ">Go Back</button>
+           </div>
         </div>
     )
 }
